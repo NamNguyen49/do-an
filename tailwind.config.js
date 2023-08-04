50568b28415cb25c412e1f5d267a42ceff56313e
Nguyễn Đức Hoài Nam <namnguyen09042001@gmail.com>
2023-08-05 00:11:05 +0700

    first commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,10 +7,10 @@ module.exports = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        foreground: 'rgba(0, 0, 0, 1)', // Màu chữ trắng
+        backgroundStart: 'rgba(214, 219, 220, 1)', // Màu nền trắng
+        backgroundEnd: 'rgba(255, 255, 255, 1)', // Màu gradient nền trắng
       },
     },
   },
