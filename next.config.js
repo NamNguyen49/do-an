ad6290b392ab715bf97f4e55b67bc90383a8631d
unknown <hukhho@gmail.com>
2023-08-29 13:11:58 +0700

    ok

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    domains: ["photos.pinksale.finance"],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
--- src/app/page.js
+++ src/app/page.js
@@ -1,30 +1,20 @@
-'use client'; // If used in Pages Router, is no need to add this line
+'use client';
 
 import React from 'react';
+import Image from 'next/image';
 import UserLayout from './components/Layout/UserLayout';
 import { ConfigProvider } from 'antd';
 import { Button, Card } from 'antd';
 const { Meta } = Card;
-
-
 import Header from './components/Header';
+import CVCard from './components/Card/CVCard';
+
 const Home = () => {
     return (
         <main >
             <ConfigProvider>
-                <UserLayout content={
-
-
-                    <Card
-                        hoverable
-                        style={{
-                            width: 240,
-                        }}
-                        cover={<img alt="example" src="https://photos.pinksale.finance/file/pinksale-logo-upload/1692701716873-e9b5323edab631aa000eabb7a8512a33.PNG" />}
-                    >
-                        <Meta title="Pham Viet Thuan Thien - CV" description="cv" />
-                    </Card>
-
+                <UserLayout content={        
+                    <CVCard />
                 } />
             </ConfigProvider>
         </main>
